ce2f3216b6e59c393977020dca618f8caaff732b
Diego Esteban Cardona <char9001@hotmail.com>
2025-08-14 17:58:40 -0500

    feat: Ajustar diseño del formulario de registro de ventas y mostrar la fecha actual

--- resources/views/app/ventas/create.blade.php
+++ resources/views/app/ventas/create.blade.php
@@ -16,19 +16,19 @@
     <form action="{{ route('ventas.store') }}" method="POST">
       @csrf
       <div class="flex flex-col gap-6 md:flex-row md:justify-between">
-        <div class="flex w-2/3 flex-col">
+        <div class="flex w-3/5 flex-col">
           <livewire:ventas.buscar-tercero />
         </div>
-        <div class="flex flex-row items-center justify-end gap-3">
+        <div class="mt-2 flex w-1/5 flex-col items-start gap-2">
+          <span class="text-gray-700">Fecha:</span>
+          <span class="font-bold">{{ now()->setTimezone('America/Bogota')->format('d/m/Y, h:i A') }}</span>
+        </div>
+        <div class="flex w-1/5 flex-row items-center justify-end gap-3">
           <a href="{{ route('ventas.create') }}" class="btn-secundary">Limpiar</a>
           <button type="submit" class="btn-primary">{{ __('Registrar Venta') }}</button>
         </div>
+      </div>
 
-      </div>
-      <div class="mt-2 flex flex-row items-center gap-2">
-        <span class="text-gray-700">Fecha:</span>
-        <span class="font-bold">{{ now()->setTimezone('America/Bogota')->format('d/m/Y, h:i A') }}</span>
-      </div>
       <div class="mt-6 border border-gray-200 p-4">
         <livewire:ventas.buscar-producto />
       </div>
