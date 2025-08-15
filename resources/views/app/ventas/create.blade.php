2afdb526111b672b844128756a0386c3632e03af
Diego Esteban Cardona <char9001@hotmail.com>
2025-08-14 19:44:30 -0500

    feat: Ajustar diseño del formulario de registro de ventas para mejorar la disposición de los elementos

--- resources/views/app/ventas/create.blade.php
+++ resources/views/app/ventas/create.blade.php
@@ -16,14 +16,14 @@
     <form action="{{ route('ventas.store') }}" method="POST">
       @csrf
       <div class="flex flex-col gap-6 md:flex-row md:justify-between">
-        <div class="flex w-3/5 flex-col">
+        <div class="flex w-[50%] flex-col 2xl:w-3/5">
           <livewire:ventas.buscar-tercero />
         </div>
-        <div class="mt-2 flex w-1/5 flex-col items-start gap-2">
+        <div class="mt-2 flex flex-col items-start gap-2 2xl:w-1/5">
           <span class="text-gray-700">Fecha:</span>
           <span class="font-bold">{{ now()->setTimezone('America/Bogota')->format('d/m/Y, h:i A') }}</span>
         </div>
-        <div class="flex w-1/5 flex-row items-center justify-end gap-3">
+        <div class="flex flex-row items-center justify-end gap-3 2xl:w-1/5">
           <a href="{{ route('ventas.create') }}" class="btn-secundary">Limpiar</a>
           <button type="submit" class="btn-primary">{{ __('Registrar Venta') }}</button>
         </div>
